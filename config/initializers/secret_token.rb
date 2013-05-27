f72dc55fa458c90d0e811e1233f4f426df5949c6
Alec Tower <alectower@gmail.com>
2013-05-27 14:44:34 -0400

    basic calendar started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails.application.config.secret_key_base = '1311cabfac6c69a375652e8690b13d4ad2ac8bb08013268df7633cbe75e1abfcc8bf3ec6d84cb58ff72669689ce48514efb20de6d9f40bce3fc0203b90caf8e8'
