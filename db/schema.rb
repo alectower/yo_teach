16e3d71e8297222d95238957ee603286ab22c14f
Alec Tower <alectower@gmail.com>
2013-06-30 13:20:40 -0400

    updated sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130629200229) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: true do |t|
     t.string   "name"
     t.date     "start_date"
