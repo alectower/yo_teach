670db7baa0f5efca3f83af9910dc2e74f5f57ef3
Alec Tower <alectower@gmail.com>
2013-09-05 18:33:56 -0400

    re committing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ rval = ActiveRecord::Base.connection.tables.sort
 rval.reject! do |tbl|
     ['schema_migrations','schema_info', 'sessions', 'public_exceptions'].include?(tbl)
 end
+
 rval.each do |t|
 	t.classify.constantize.delete_all
 end
