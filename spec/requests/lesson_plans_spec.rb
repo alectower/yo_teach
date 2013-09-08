ae12d6eb10708e34eb114df065d54e1c36a20e90
Alec Tower <alectower@gmail.com>
2013-09-08 15:14:30 -0400

    updated lesson plan index wording
    - added request spec for creating a lesson plan

--- spec/requests/lesson_plans_spec.rb
+++ spec/requests/lesson_plans_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 describe "LessonPlans" do
 
+	describe "Create new lesson plan" do
+		context "course exists" do 
+			it "adds a new lesson plan and displays results" do
+				FactoryGirl.create(:course, name: 'Math')
+				visit lesson_plans_path
+				page.should have_content /Lesson Plan Description/
+				page.should have_content /Course/
+				page.should have_content /Date/
+				page.should have_content /Start Time/
+				page.should have_content /End Time/
+				expect {
+					click_link 'New Lesson Plan'
+					select 'Math', from: 'Course'
+					fill_in 'Description', with: 'Mitosis'
+					fill_in 'Start', with: DateTime.now
+					fill_in 'End', with: 1.hour.since(DateTime.now)
+					click_button 'Create Lesson Plan'
+				}.to change(LessonPlan, :count).by(1)
+			end
+		end
+	end
+
   describe "GET /lesson_plans/new" do
 		it "provides a description field" do
 			visit new_lesson_plan_path
