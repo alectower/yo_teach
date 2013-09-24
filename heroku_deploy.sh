18226269c92eb005a6e390ce6dd16ecdf7680ff8
Alec Tower <alectower@gmail.com>
2013-09-24 18:17:29 -0400

    updated deploy script

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -e
  
+bundle install
+
 rake assets:precompile RAILS_ENV=production
 
+git add -f Gemfile.lock
 git add public/assets
 git commit -m 'Precompiling assets for heroku'
  
-git push heroku master
+git push -f heroku master
  
 git rm -r public/assets
+git rm -f Gemfile.lock
 git commit -m 'Removing compiled assets'
+
