c038657a55f14b6c2eaf158ac2fb46cd3c1e184a
Alec Tower <alectower@gmail.com>
2013-09-24 18:20:27 -0400

    removing assets

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
  
 bundle install
+git add -f Gemfile.lock
 
 rake assets:precompile RAILS_ENV=production
 
-git add -f Gemfile.lock
 git add public/assets
 git commit -m 'Precompiling assets for heroku'
  
 git push -f heroku master
  
-git rm -r public/assets
+git rm -rf public/assets
 git rm -f Gemfile.lock
 git commit -m 'Removing compiled assets'
 
