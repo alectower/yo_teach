c9cd0ccd86fee0a22d06d11ffd8cf037445399c8
Alec Tower <alectower@gmail.com>
2013-09-25 17:16:18 -0400

    readded of Gemfile.lock

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -e
  
-bundle install
-git add -f Gemfile.lock
-
 rake assets:precompile RAILS_ENV=production
 
 git add public/assets
@@ -11,6 +8,5 @@ git commit -m 'Precompiling assets for heroku'
 git push -f heroku master
  
 git rm -rf public/assets
-git rm -f Gemfile.lock
 git commit -m 'Removing compiled assets'
 
