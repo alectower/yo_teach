c44b7be15bb174b040682b2756e0e039aaa05b3f
Luke Tower <lukegtower@gmail.com>
2013-10-20 18:57:36 -0400

    Add delete lesson plan field option

---
+++ app/controllers/lesson_plan_fields_controller.rb
@@ -0,0 +1,18 @@
+class LessonPlanFieldsController < ApplicationController
+
+  def destroy
+    @lesson_plan_field = LessonPlanField.find(params[:id])
+    if @lesson_plan_field.destroy
+      flash[:success] = "You have removed #{@lesson_plan_field.title}
+                         from the lesson plan."
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.json { head :no_content }
+      end
+    else
+      flash[:error] = "There was a problem with your request."
+      render :back
+    end
+
+  end
+end
