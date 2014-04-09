7ad242bf4616be355fcddb582d741aa3d9994a31
Alec Tower <alectower@gmail.com>
2014-04-09 09:56:25 -0400

    Lesson plan field notice wording update

--- app/controllers/lesson_plan_fields_controller.rb
+++ app/controllers/lesson_plan_fields_controller.rb
@@ -3,9 +3,9 @@ class LessonPlanFieldsController < ApplicationController
   def destroy
     @lesson_plan_field = LessonPlanField.find(params[:id])
     if @lesson_plan_field.destroy
-      flash[:notice] = "#{@lesson_plan_field.title} has been removed from the lesson plan."
+      flash[:notice] = "#{@lesson_plan_field.title} field has been removed from the lesson plan."
     else
-      flash[:error] = "#{@lesson_plan_field.title} failed to be removed from the lesson plan."
+      flash[:error] = "#{@lesson_plan_field.title} field failed to be removed from the lesson plan."
     end
     redirect_to :back
   end
