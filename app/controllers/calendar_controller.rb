05aaf6d1755f396c45b82fdca1ae69d63cd7e44b
Alec Tower <alectower@gmail.com>
2014-04-10 12:27:12 -0400

    Calendar controller params security update

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,12 +1,22 @@
 class CalendarController < ApplicationController
 
   def calendar
-    view = params.fetch(:view) { :month }
+    view = view_params[:view] || :month
     calendar = Calendar.new calendar_date, view: view
     render view, locals: { calendar: calendar }
   end
 
   private
+
+  def view_params
+    permitted = []
+    permitted << :view if is_view_param?
+    params.permit *permitted
+  end
+
+  def is_view_param?
+    %w(week month).include? params[:view]
+  end
 
   def calendar_date
     params[:date] ? Date.strptime(params[:date], "%m/%d/%Y") : Date.today
