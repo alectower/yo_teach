fa748685660053a55dfb2c1c702e256a11ff900b
Alec Tower <alectower@gmail.com>
2014-04-30 15:52:57 -0400

    Cascade delete for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_format_of :email, with: /\A.+@.+\..+\Z/
 
-  has_one :account
-  has_many :courses
-  has_many :lesson_plans
-  has_many :to_dos
+  has_many :courses, dependent: :destroy
+  has_many :lesson_plans, dependent: :destroy
+  has_many :to_dos, dependent: :destroy
 end
