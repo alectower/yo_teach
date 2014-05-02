38660e6b81702cabe616d7c71967bddb1fdf1e96
Alec Tower <alectower@gmail.com>
2014-05-01 23:32:31 -0400

    Added name of email sender and recipient

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,27 +1,32 @@
 class UserMailer < ActionMailer::Base
-  default from: I18n.t(:noreply_email)
+  default from:
+    "#{I18n.t :app_name} <#{I18n.t :noreply_email}>"
 
   def new_user(user)
     @user = user
-    mail to: user.email, subject:
-      "#{t :app_name} sign up"
+    mail_to_user "#{t :app_name} sign up"
   end
 
   def user_update(user)
     @user = user
-    mail to: user.email, subject:
-      "#{t :app_name} user update"
+    mail_to_user "#{t :app_name} user update"
   end
 
   def password_update(user)
     @user = user
-    mail to: user.email, subject:
-      "#{t :app_name} password update"
+    mail_to_user "#{t :app_name} password update"
   end
 
   def password_reset(user)
+    @user = user
     @token = user.password_reset_token
-    mail to: user.email, subject:
-      "#{t :app_name} password reset"
+    mail_to_user "#{t :app_name} password reset"
+  end
+
+  private
+
+  def mail_to_user(subject)
+    mail to: "#{@user.name} <#{@user.email}>",
+      subject: subject
   end
 end
